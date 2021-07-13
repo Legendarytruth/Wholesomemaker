50886457b6484001e920991c4ee2d89a09b7936f
Dennys Matthew <nmatthewsoft@gmail.com>
2021-07-13 15:08:46 +0700

    Adding Slash Commands Wrapper for ban modules

--- cogs/ban.py
+++ cogs/ban.py
@@ -1,37 +1,54 @@
 import discord
+import discord_slash.cog_ext
 from discord.ext import commands
 from discord import Embed
 from typing import Optional
 import datetime
 import asyncio
+from discord_slash import *
+from discord_slash.utils.manage_commands import create_option
 
 
 class ban(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
-    @commands.has_permissions(ban_members=True)
-    async def ban(self, ctx, user_id, *, reason: Optional[str] = "No reason provided."):
+    @cog_ext.cog_slash(name="ban",
+                       description="Ban a user.",
+                       options=[
+                           create_option(
+                               name="user_id",
+                               description="Who is the Person Should I Ban? (UserID Only)",
+                               option_type=3,
+                               required=True
+                           ),
+                           create_option(
+                               name="reason",
+                               description="Provide the Reason (Optional)",
+                               option_type=3,
+                               required=False
+                           )
+                       ])
+    @commands.has_role(845586428057354253)  # dispatch role (head moderators)
+    async def ban(self, ctx: SlashContext, user_id, *, reason: Optional[str] = "No reason provided."):
         user = await self.client.fetch_user(user_id)
-        await ctx.message.delete()
         try:
             await user.send(f'You have been banned from **{ctx.guild}** for the following reason: {reason}')
         except:
             embed = discord.Embed(
-                    description=f"<:cross:839158779815657512> Failed to send DM to <@{user_id}>. They probably had their DM's closed.")
-            await ctx.channel.send(embed=embed)
+                description=f"<:cross:839158779815657512> Failed to send DM to <@{user_id}>. They probably had their DM's closed.")
+            await ctx.send(embed=embed)
         await ctx.guild.ban(user, reason=reason)
         embed = discord.Embed(
-                    description=f"<:check:839158727512293406> Successfully banned {user} for: {reason}")
-        await ctx.channel.send(embed=embed)
+            description=f"<:check:839158727512293406> Successfully banned {user} for: {reason}")
+        await ctx.send(embed=embed)
 
     @ban.error
     async def ban_error(self, ctx, error):
-        if isinstance(error, commands.MissingPermissions):
+        if isinstance(error, commands.MissingRole):
             embed = discord.Embed(
-                    description=f"<:cross:839158779815657512> You must have the `Ban Members` permission to use this command!")
-            await ctx.channel.send(embed=embed)
+                description=f"<:cross:839158779815657512> You must have the <@&845586428057354253> roles to use this command!")
+            await ctx.send(embed=embed)
 
 
 def setup(client):
