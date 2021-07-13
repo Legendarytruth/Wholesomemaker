0b451984f020d20646c0155371642ffb8f3f7729
Dennys Matthew <nmatthewsoft@gmail.com>
2021-07-13 15:08:59 +0700

    Adding Slash Commands Wrapper for dog modules

--- cogs/dog.py
+++ cogs/dog.py
@@ -1,33 +1,36 @@
 import discord
+import discord_slash.cog_ext
 from discord.ext import commands
 from discord import Embed
 from typing import Optional
 import datetime
 import asyncio
 import aiohttp
+from discord_slash import *
+from discord_slash.utils.manage_commands import create_option
 
 
 class dog(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
-    async def dog(self, ctx):
+    @cog_ext.cog_slash(name="dog", description="Provide you with some Dog Pics and Facts.")
+    async def dog(self, ctx: SlashContext):
         async with aiohttp.ClientSession() as session:
             # Make a request
-            request = await session.get('https://some-random-api.ml/img/dog')
+            request = await session.get('https://dog.ceo/api/breeds/image/random')
             dogjson = await request.json()  # Convert it to a JSON dictionary
         # This time we'll get the fact request as well!
             request2 = await session.get('https://some-random-api.ml/facts/dog')
             factjson = await request2.json()
         embed = discord.Embed(
-            title="Doggo! <a:rooCute:839162904414257162>", color=discord.Color.purple())  # Create embed
+            title=":dog: Doggo!", color=discord.Color.purple())  # Create embed
         # Set the embed image to the value of the 'link' key
         embed.add_field(name="Did you know?",
                         value=f"{factjson['fact']}", inline=False)
-        embed.set_image(url=dogjson['link'])
+        embed.set_image(url=dogjson['message'])
         embed.set_footer(
-            text=f"Powered by some-random-api.ml API😉")
+            text=f"Powered by some-random-api.ml and dog.ceo API😉")
         await ctx.send(embed=embed)  # Send the embed
 
 
