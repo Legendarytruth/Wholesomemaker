7d69ac9152d460806d2bbd72d2357b63ead86415
Dennys Matthew <nmatthewsoft@gmail.com>
2021-07-13 15:10:02 +0700

    Adding Slash Commands Wrapper for mysteryspin modules

--- cogs/mysteryspin.py
+++ cogs/mysteryspin.py
@@ -1,5 +1,8 @@
 import discord
+import discord_slash.cog_ext
 from discord.ext import commands
+from discord_slash import *
+from discord_slash.utils.manage_commands import create_option
 from discord import Embed
 from typing import Optional
 import datetime
@@ -14,9 +17,18 @@ class mysteryspin(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
-    @commands.has_permissions(kick_members=True)
-    async def mysteryspin(self, ctx, *, member: discord.Member = None):
+    @cog_ext.cog_slash(name="mysteryspin",
+                       description="Spin the Mystery Wheel and Get More Booming Prize!",
+                       options=[
+                           create_option(
+                               name="member",
+                               description="Who is the Person?",
+                               option_type=6,
+                               required=False
+                           )
+                       ])
+    @commands.has_role(825578057498099732)
+    async def mysteryspin(self, ctx: SlashContext, *, member: discord.Member = None):
 
         if member is None:
             member = ctx.author
@@ -38,6 +50,9 @@ class mysteryspin(commands.Cog):
                f"{member.mention} spinned the Mystery Spin and got All Snacks Replenished. \n You can spin the wheel again in 24h.",
                f"{member.mention} spinned the Mystery Spin and got Resupplied Bunker. \n You can spin the wheel again in 24h.",
                f"{member.mention} spinned the Mystery Spin and hit the Fucking Jackpot and got 100,000XP <:FennPOG:839189820668248135>. \n XP will be awarded within 24h. \n You can spin the wheel again in 24h.",
+               f"{member.mention} spinned the Mystery Spin and hit the Fucking Fuck Jackpot and got 150,000XP <:FennPOG:839189820668248135>. \n XP will be awarded within 24h. \n You can spin the wheel again in 24h.",
+               f"{member.mention} spinned the Mystery Spin and hit the Fucking Fucker Fuck Jackpot and got 200,000XP <:FennPOG:839189820668248135>. \n XP will be awarded within 24h. \n You can spin the wheel again in 24h.",
+               f"{member.mention} spinned the Mystery Spin and hit the Pogn't Jackpot and got XP Reset <:FennPOG:839189820668248135>. \n XP will be resetted within 24h. \n You can spin the wheel again in 24h.",
                ]
 
         if ctx.channel.id == bot_channel:
@@ -46,10 +61,17 @@ class mysteryspin(commands.Cog):
             #    description=f'{random.choice(ans)}', colour=discord.Colour.green())
             # embed.set_footer(
             #    text=f"Congratulations, Winners of Mystery Wheel!", icon_url="https://cdn.discordapp.com/emojis/816783243109335100.png?v=1")
-            await ctx.reply(f"{random.choice(ans)}", mention_author=False)
+            await ctx.send(f"{random.choice(ans)}")
         else:
             embed = discord.Embed(
                 description=f"<:cross:839158779815657512> **{ctx.author.name}**, that command is disabled in this channel.")
+            await ctx.channel.send(embed=embed)
+
+    @mysteryspin.error
+    async def mysteryspin_error(self, ctx, error):
+        if isinstance(error, commands.MissingRole):
+            embed = discord.Embed(
+                description=f"<:cross:839158779815657512> You must have the <@&825578057498099732> roles to use this command!")
             await ctx.channel.send(embed=embed)
 
 
