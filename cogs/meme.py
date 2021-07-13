26be15c3d2e683e46123d3da53c24d161ee001be
Dennys Matthew <nmatthewsoft@gmail.com>
2021-07-13 15:09:42 +0700

    Adding Slash Commands Wrapper for meme modules

--- cogs/meme.py
+++ cogs/meme.py
@@ -1,5 +1,8 @@
 import discord
+import discord_slash.cog_ext
 from discord.ext import commands
+from discord_slash import *
+from discord_slash.utils.manage_commands import create_option
 from discord import Embed
 from typing import Optional
 import datetime
@@ -11,8 +14,8 @@ class meme(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
-    async def meme(self, ctx):
+    @cog_ext.cog_slash(name="meme", description="🤣🤣")
+    async def meme(self, ctx: SlashContext):
         async with aiohttp.ClientSession() as session:
             # Make a request
             request = await session.get('https://some-random-api.ml/meme')
