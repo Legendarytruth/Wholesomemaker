a335be380f82c10bbdfd0b60069a251feef6b8f5
Dennys Matthew <nmatthewsoft@gmail.com>
2021-07-13 15:11:56 +0700

    Adding Slash Commands Wrapper for unmute modules

--- cogs/unmute.py
+++ cogs/unmute.py
@@ -1,5 +1,8 @@
 import discord
+import discord_slash.cog_ext
 from discord.ext import commands
+from discord_slash import *
+from discord_slash.utils.manage_commands import create_option
 from discord import Embed
 from typing import Optional
 import datetime
@@ -10,24 +13,30 @@ class unmute(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
-    @commands.has_permissions(kick_members=True)
-    async def unmute(self, ctx, member: discord.Member):
-        if member is None:
-            await ctx.send('Please pass in a valid user by mentioning them or using their ID.')
-            return
+    @cog_ext.cog_slash(name="unmute",
+                       description="Unmute a user.",
+                       options=[
+                           create_option(
+                               name="member",
+                               description="Who is the Person Should I Unmute?",
+                               option_type=6,
+                               required=True
+                           )
+                       ])
+    @commands.has_role(825578057498099732)
+    async def unmute(self, ctx: SlashContext, member: discord.Member):
         role = discord.utils.get(ctx.guild.roles, name='Muted')
         await member.remove_roles(role)
         embed = discord.Embed(
             description=f"<:check:839158727512293406> Successfully unmuted {member.mention}.")
-        await ctx.channel.send(embed=embed)
+        await ctx.send(embed=embed)
 
     @unmute.error
     async def unmute_error(self, ctx, error):
-        if isinstance(error, commands.MissingPermissions):
+        if isinstance(error, commands.MissingRole):
             embed = discord.Embed(
-                description=f"'<:cross:839158779815657512> You must have the `Kick Members` permission to use this command!")
-            await ctx.channel.send(embed=embed)
+                description=f"<:cross:839158779815657512> You must have the <@&825578057498099732> roles to use this command!")
+            await ctx.send(embed=embed)
 
 
 def setup(client):
