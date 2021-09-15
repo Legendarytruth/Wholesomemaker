15430b6bf41768af76c7bb43b7b935ddb84a7c83
Dennys Matthew <nmatthewsoft@gmail.com>
2021-09-15 10:08:58 +0700

    lowering wheelspin cooldowns

--- cogs/wheelspin.py
+++ cogs/wheelspin.py
@@ -19,61 +19,54 @@ class wheelspin(commands.Cog):
         self.client = client
 
     @cog_ext.cog_slash(name="wheelspin", description="Spin the wheel and Get some Prize!")
-    @commands.cooldown(1, 21600, commands.BucketType.user)
+    @commands.cooldown(1, 10800, commands.BucketType.user)
     async def wheelspin(self, ctx: SlashContext):
-        ans = [f"{ctx.author.mention} spinned the Lucky Wheel and got Sent to Kamurocho. \n You can spin the wheel again in 6h.",
-               #f"{ctx.author.mention} spinned the Lucky Wheel and got Muted for 10 Minutes <:GTASeriesVideos:839162693415206962>. \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got Sent to Sotenbori. \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got Free Round Trip to Cayo Perico. \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got 10 P’s & Q’s Snacks. \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got 10 Meteorite Snacks. \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got 10 EgoChaser Snacks. \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got 5 Pißwasser Drinks. \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got 5 eCola Drinks. \n You can spin the wheel again in 6h.",
-               #f"{ctx.author.mention} spinned the Lucky Wheel and got Pack of Redwood. \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got 500XP. \n Your XP will be awarded within 6h. \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got clothing <:clothing:839192909583613982>. \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got arrested by the FBI <a:FlashingWanted:839193592999444520>. \n You can spin the wheel again in 6h.",
-               #f"{ctx.author.mention} spinned the Lucky Wheel and got a slap <a:Slap:839192442199867422>. \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got 100XP. \n Your XP will be awarded within 6h. \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got 50XP. \n XP will be awarded within 6h.  \n You can spin the wheel again in 6h.",
-               #f"{ctx.author.mention} spinned the Lucky Wheel and got champagne <:TomConnors:839192145482612776>. \n You can spin the wheel again in 6h.",
-               #f"{ctx.author.mention} spinned the Lucky Wheel and got nothing, get trolled <:GTASeriesVideos:839162693415206962>. \n You can spin the wheel again in 6h.",
-               #f"{ctx.author.mention} spinned the Lucky Wheel and got 2kRP <:Kapp:839190045767630899>. \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got Ghost Organization <:CoolBread:839191670767616011> \n You are now hidden from other users. \n To use this, please ask an operator to add the role to you. \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got 7500XP <:FennPOG:839189820668248135>. \n Your XP will be awarded within 6h. \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got 70XP. \n Your XP will be awarded within 6h. \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and hit the Jackpot and got 20,000XP <:FennPOG:839189820668248135>. \n XP will be awarded within 6h. \n You can spin the wheel again in 6h.",
-               #f"{ctx.author.mention} spinned the Lucky Wheel and got a bonk on the head <a:Bonk:839190543095169055>. \n You can spin the wheel again in 6h.",
-               #f"{ctx.author.mention} spinned the Lucky Wheel and got bread <:Bread:839190517555265606>. \n You can spin the wheel again in 6h.",
-               #f"{ctx.author.mention} spinned the Lucky Wheel and got 1XP. XP will be awarded within 6h. \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got the podium vehicle + 5,000XP <:CoolBread:839191670767616011>. \n XP will be awarded within 6h. \n You can spin the wheel again in 6h.",
-               #f"{ctx.author.mention} spinned the Lucky Wheel and got the podium vehicle. \n You can spin the wheel again in 6h.",
-               #f"{ctx.author.mention} spinned the Lucky Wheel and got $50,000 \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got Mystery Spin <:CoolBread:839191670767616011> \n To use this, please ask an operator to spin the Mystery Spin. \n You can spin the wheel again in 6h.",
-               #f"{ctx.author.mention} spinned the Lucky Wheel and got 10% Vehicle Discount <:Kapp:839190045767630899> \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got Full Ammo \n You can spin the wheel again in 6h.",
-               #f"{ctx.author.mention} spinned the Lucky Wheel and got $25,000. \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got 5 eCola Drinks. \n You can spin the wheel again in 6h.",
-               #f"{ctx.author.mention} spinned the Lucky Wheel and got 25.000 Chips. \n You can spin the wheel again in 6h.",
-               #f"{ctx.author.mention} spinned the Lucky Wheel and got A Random Special Cargo Item. \n You can spin the wheel again in 6h.",
-               #f"{ctx.author.mention} spinned the Lucky Wheel and got All Snacks Replenished. \n You can spin the wheel again in 6h.",
-               #f"{ctx.author.mention} spinned the Lucky Wheel and got Resupplied Bunker. \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got Mystery Spin + 5,000 XP <:CoolBread:839191670767616011> \n To use this, please ask an operator to spin the Mystery Spin. \n XP will be awarded within 6h. \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got Mystery Spin + 10,000 XP <:CoolBread:839191670767616011> \n To use this, please ask an operator to spin the Mystery Spin. \n XP will be awarded within 6h. \n You can spin the wheel again in 6h.",
-               f"{ctx.author.mention} spinned the Lucky Wheel and got Mystery Spin + 20,000 XP <:CoolBread:839191670767616011> \n To use this, please ask an operator to spin the Mystery Spin. \n XP will be awarded within 6h. \n You can spin the wheel again in 6h.",
+        ans = [f"{ctx.author.mention} spinned the Lucky Wheel and got Sent to Kamurocho. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got Muted for 10 Minutes <:GTASeriesVideos:839162693415206962>. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got Sent to Sotenbori. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got Free Round Trip to Cayo Perico. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got 10 P’s & Q’s Snacks. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got 10 Meteorite Snacks. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got 10 EgoChaser Snacks. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got 5 Pißwasser Drinks. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got 5 eCola Drinks. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got Pack of Redwood. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got 500XP. \n Your XP will be awarded within 3h. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got clothing <:clothing:839192909583613982>. \n You can spin the wheel again in 3h.",
+               #f"{ctx.author.mention} spinned the Lucky Wheel and got arrested by the FBI <a:FlashingWanted:839193592999444520>. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got a slap <a:Slap:839192442199867422>. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got 100XP. \n Your XP will be awarded within 3h. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got 50XP. \n XP will be awarded within 3h.  \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got champagne <:TomConnors:839192145482612776>. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got nothing, get trolled <:GTASeriesVideos:839162693415206962>. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got 2kRP <:Kapp:839190045767630899>. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got Ghost Organization <:CoolBread:839191670767616011> \n You are now hidden from other users. \n To use this, please ask an operator to add the role to you. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got 7500XP <:FennPOG:839189820668248135>. \n Your XP will be awarded within 3h. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got 70XP. \n Your XP will be awarded within 3h. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and hit the Jackpot and got 20,000XP <:FennPOG:839189820668248135>. \n XP will be awarded within 3h. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got a bonk on the head <a:Bonk:839190543095169055>. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got bread <:Bread:839190517555265606>. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got 1XP. XP will be awarded within 3h. \n You can spin the wheel again in 3h.",
+               #f"{ctx.author.mention} spinned the Lucky Wheel and hit the Fucking Jackpot and got 25,000XP <:FennPOG:839189820668248135> \n XP will be awarded within 3h. \n You can spin the wheel again in 3h.",
+               #f"{ctx.author.mention} spinned the Lucky Wheel and hit the Fucking Fuck Jackpot and got 35,000XP <:FennPOG:839189820668248135> \n XP will be awarded within 3h. \n You can spin the wheel again in 3h.",
+               #f"{ctx.author.mention} spinned the Lucky Wheel and hit the Fucking Fucker Fuck Jackpot and got 50,000XP <:FennPOG:839189820668248135> \n XP will be awarded within 3h. \n You can spin the wheel again in 3h.",
+               #f"{ctx.author.mention} spinned the Lucky Wheel and got the podium vehicle. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got $50,000 \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got 10% Vehicle Discount <:Kapp:839190045767630899> \n You can spin the wheel again in 3h.",
+               #f"{ctx.author.mention} spinned the Lucky Wheel and got Full Ammo \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and go- *an unknown has placed a $9000 bounty on you!* <:mmmonkas:876174042044981289> \n You can spin the wheel again in 3h.",
+               #f"{ctx.author.mention} spinned the Lucky Wheel and got 25.000 Chips. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got 5 Taco :taco: \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got 10 Taco :taco: \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got A Random Special Cargo Item. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got All Snacks Replenished. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got Resupplied Bunker. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got 5,000 XP <:FennPOG:839189820668248135> \n XP will be awarded within 3h. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got 10,000 XP <:FennPOG:839189820668248135> \n XP will be awarded within 3h. \n You can spin the wheel again in 3h.",
+               f"{ctx.author.mention} spinned the Lucky Wheel and got 20,000 XP <:FennPOG:839189820668248135> \n XP will be awarded within 3h. \n You can spin the wheel again in 3h.",
                ]
         if ctx.channel.id == bot_channel:
-            #embed = discord.Embed(colour=discord.Colour.green())
-            # embed = discord.Embed(
-            #    description=f'{random.choice(ans)}', colour=discord.Colour.green())
-            # embed.set_footer(
-            #    text=f"Credits to ZappiestSet81#1999 for the Responses, Love you Homie! ❤️", icon_url="https://cdn.discordapp.com/attachments/826162439799308350/832703090939265024/pet.gif")
             await ctx.send(f'{random.choice(ans)}')
-        # else:
-            # embed = discord.Embed(
-            #    description=f"<:cross:839158779815657512> **{ctx.author.name}** that command is disabled in this channel.")
-            # await ctx.channel.send(embed=embed)
 
     def better_time(self, cd: int):
         time = f"{cd} seconds"
