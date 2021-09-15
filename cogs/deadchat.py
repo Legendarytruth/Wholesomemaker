2dedcdea49ff633e740a6d57239db97e01e53e8a
Dennys Matthew <nmatthewsoft@gmail.com>
2021-09-15 10:07:21 +0700

    Update deadchat.py

--- cogs/deadchat.py
+++ cogs/deadchat.py
@@ -11,8 +11,10 @@ class deadchat(commands.Cog):
     @commands.guild_only()
     @commands.Cog.listener()
     async def on_message(self, ctx):
-        if ctx.content.startswith('dead chat'):
-            await ctx.reply(f"If the chat is dead, it means that people have better things to do than keep it alive. How about you {ctx.author.name}? Why don't you keep it alive?", mention_author=False)
+        kung = ('dead chat', 'deadchat', 'dchat', 'ded chat',
+                'ded chat xd', 'dead chat xd', 'dedchat xd', 'deadchat xd')
+        if ctx.content.startswith(kung):
+            await ctx.reply(f"If the chat is dead, it means that people have better things to do than keep it alive. How about you {ctx.author.display_name}? Why don't you keep it alive?", mention_author=False)
 
 
 def setup(client):
